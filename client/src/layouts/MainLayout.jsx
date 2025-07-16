8ba133aa4c15903840d4d4a3bcb7049da8bd3bef
Miqdad <miqdad.shaldin@gmail.com>
2025-07-16 22:44:12 +0700

    install tailwind and daisyUI, make navbar for homepage

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet, useNavigate } from "react-router";
-import { onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../configs/firebase";
 import { useEffect, useState } from "react";
 
@@ -21,9 +21,97 @@ export default function MainLayout() {
     return <div>Loading...</div>;
   }
 
+  async function handleLogOut() {
+    try {
+      signOut(auth);
+      navigate("/auth");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <>
-      <header> --- Halaman Utama --- </header>
+      <nav className="bg-base-100 shadow-sm mb-5">
+        <div className="navbar m-auto max-w-7xl">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  {" "}
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />{" "}
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              >
+                <li>
+                  <a>Item 1</a>
+                </li>
+                <li>
+                  <a>Parent</a>
+                  <ul className="p-2">
+                    <li>
+                      <a>Submenu 1</a>
+                    </li>
+                    <li>
+                      <a>Submenu 2</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a>Item 3</a>
+                </li>
+              </ul>
+            </div>
+            <a className="btn btn-ghost text-xl">daisyUI</a>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li>
+                <details>
+                  <summary>Parent</summary>
+                  <ul className="p-2">
+                    <li>
+                      <a>Submenu 1</a>
+                    </li>
+                    <li>
+                      <a>Submenu 2</a>
+                    </li>
+                  </ul>
+                </details>
+              </li>
+              <li>
+                <a>Item 3</a>
+              </li>
+            </ul>
+          </div>
+          <div className="navbar-end">
+            <a className="btn" onClick={handleLogOut}>
+              Logout
+            </a>
+          </div>
+        </div>
+      </nav>
       <Outlet />
     </>
   );
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,27 +1,11 @@
-import { signOut } from "firebase/auth";
-import { auth } from "../configs/firebase";
-import { useNavigate } from "react-router";
-
 export default function HomePage() {
-  const navigate = useNavigate();
-
-  async function handleLogOut() {
-    try {
-      signOut(auth);
-      navigate("/auth");
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
   return (
     <>
-      <h1>Halo dunia</h1>
+      <h1 className="font-bold text-3xl">Halo dunia</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
         dicta officia autem sequi molestiae maiores.
       </p>
-      <button onClick={handleLogOut}>Logout</button>
     </>
   );
 }
