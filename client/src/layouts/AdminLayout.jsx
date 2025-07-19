f3be8abf6cff36b18a4a67174f43b1872971f5e8
Miqdad <miqdad.shaldin@gmail.com>
2025-07-19 23:34:56 +0700

    edit logo daisyUI in admin layout navbar

--- client/src/layouts/AdminLayout.jsx
+++ client/src/layouts/AdminLayout.jsx
@@ -101,7 +101,7 @@ export default function AdminLayout() {
                 </li>
               </ul>
             </div>
-            <a className="btn btn-ghost text-xl">daisyUI</a>
+            <a className="btn btn-ghost text-xl">Health & Medicine</a>
           </div>
           <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal px-1">
