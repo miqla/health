189a072a135725e2de0bb2a695f4f1f8bf9d5c04
Miqdad <miqdad.shaldin@gmail.com>
2025-07-25 00:07:13 +0700

    add the add button

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -22,38 +22,40 @@ export default function HomePage() {
 
   return (
     <>
-      <h1 className="font-bold text-3xl text-center mb-4">Daftar Produk</h1>
-      {/* tinggal tampilkan produk dengan tabel dibawah ini, lanjut besok */}
-      <div className="mx-auto max-w-6xl overflow-x-auto rounded-box border border-base-content/5 bg-base-100 w-9/10">
-        <table className="table">
-          {/* head */}
-          <thead>
-            <tr>
-              <th>No</th>
-              <th>Name</th>
-              <th>Image</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* looping rows */}
-            {products?.map((product, index) => (
-              <tr key={product.id}>
-                <th>{index + 1}</th>
-                <td>{product.name}</td>
-                <td>
-                  <img
-                    width="150px"
-                    src={product.imageUrl}
-                    alt={product.name}
-                  />
-                </td>
-                <td>{product.price}</td>
+      <main className="mx-auto max-w-6xl w-9/10 flex flex-col">
+        <h1 className="font-bold text-3xl text-center mb-3">Daftar Produk</h1>
+        <button className="btn self-end mb-2 w-max">Add Data</button>
+        <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100 ">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Name</th>
+                <th>Image</th>
+                <th>Price</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {/* looping rows */}
+              {products?.map((product, index) => (
+                <tr key={product.id}>
+                  <th>{index + 1}</th>
+                  <td>{product.name}</td>
+                  <td>
+                    <img
+                      width="150px"
+                      src={product.imageUrl}
+                      alt={product.name}
+                    />
+                  </td>
+                  <td>{product.price}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </main>
     </>
   );
 }
