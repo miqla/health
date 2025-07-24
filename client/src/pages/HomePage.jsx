559094de985a9ed4c76fc67806452d08735505f7
Miqdad <miqdad.shaldin@gmail.com>
2025-07-24 23:40:42 +0700

    read data from database

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -17,18 +17,43 @@ export default function HomePage() {
       });
       setProducts(result);
     }
-
     getProducts();
   }, []);
 
   return (
     <>
-      <h1 className="font-bold text-3xl">Daftar Produk</h1>
+      <h1 className="font-bold text-3xl text-center mb-4">Daftar Produk</h1>
       {/* tinggal tampilkan produk dengan tabel dibawah ini, lanjut besok */}
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-        dicta officia autem sequi molestiae maiores.
-      </p>
+      <div className="mx-auto max-w-6xl overflow-x-auto rounded-box border border-base-content/5 bg-base-100 w-9/10">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Image</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* looping rows */}
+            {products?.map((product, index) => (
+              <tr key={product.id}>
+                <th>{index + 1}</th>
+                <td>{product.name}</td>
+                <td>
+                  <img
+                    width="150px"
+                    src={product.imageUrl}
+                    alt={product.name}
+                  />
+                </td>
+                <td>{product.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
