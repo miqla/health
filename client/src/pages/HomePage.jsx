4dd3cba21ca60c15171bdb93022bcf01aeb04f45
Miqdad <miqdad.shaldin@gmail.com>
2025-07-26 00:08:54 +0700

    feat: enable add function

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -34,7 +34,7 @@ export default function HomePage() {
         </button>
         <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100 ">
           <table className="table">
-            {/* head */})
+            {/* head */}
             <thead>
               <tr>
                 <th>No</th>
